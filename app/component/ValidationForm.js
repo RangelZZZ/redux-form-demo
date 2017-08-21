b6a3f7ae6dbb54a789ef819d957e85f315aa8fa9
rangelZZZ <18792561236@163.com>
2017-08-21 23:20:24 +0800

    redux-form-demo

---
+++ app/component/ValidationForm.js
@@ -0,0 +1,49 @@
+import React from "react"
+import {Field, reduxForm} from "redux-form";
+
+const required = value => value ? undefined : "can't  empty";
+const maxLength = (max, value) => value && value.length > 15 ? `Must be ${max} characters or less` : undefined;
+
+const renderField = ({
+                         input,
+                         label,
+                         type,
+                         meta: {touched, error, warning}
+                     }) => (
+    <div>
+        <label>
+            {label}
+        </label>
+        <div>
+            <input {...input} type={type} placeholder={type}/>
+            {touched && ((error && <span>{error}</span> || warning && <span>{warning}</span>))}
+        </div>
+    </div>
+);
+
+const ValidateForm = (props) => {
+    const {handleSubmit, pristine, reset, submitting} = props;
+    /* pristine if the value as the initial state*/
+
+    return <form onSubmit={handleSubmit(values => console.log(values))}>
+        <Field name="username"
+               type="text"
+               component={renderField}
+               validate={[required, maxLength]}
+        />
+        <div>
+            <button type="submit" disabled={submitting}>Submit</button>
+            <button type="button" disabled={pristine || submitting} onClick={reset}>Clear Values</button>
+        </div>
+    </form>
+
+};
+
+export default reduxForm({
+    form: "validateForm",       /*为什么要这个东西*/
+
+})(ValidateForm)
+
+
+
+
---
+++ app/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {createStore, applyMiddleware} from 'redux'
+import {Provider} from 'react-redux'
+import ContactForm from './component/ValidationForm'
+import reducers from './reducer/index'
+import {composeWithDevTools} from 'redux-devtools-extension'
+import thunkMiddleware from 'redux-thunk'
+
+const store = createStore(reducers, composeWithDevTools(
+    applyMiddleware(thunkMiddleware),
+));
+
+ReactDOM.render(
+    <Provider store={store}>
+        <ContactForm />
+    </Provider>,
+    document.getElementById('root')
+);
